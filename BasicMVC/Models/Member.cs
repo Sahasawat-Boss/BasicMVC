18cbcb62e514c8c55eac2d473ab68440e6cd0f8a
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-05-16 16:48:46 +0700

    a Member Class

---
+++ BasicMVC/Models/Member.cs
@@ -0,0 +1,12 @@
+﻿namespace BasicMVC.Models
+{
+    public class Member
+    //Class Member สำหรับเก็บข้อมูลชั่วคราว
+    {
+        //สร้างคุณสมบัติ: Type "Prop" and click Tab
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+
+    }
+}
